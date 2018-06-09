143f1240e6fe92acd412cb0242537e72e37df0d6
11486 <11486@192.168.1.103>
2018-06-09 21:35:11 +0800

    PracticOne

---
+++ src/test/java/com/selenium/PracticOne/HellowWorld.java
@@ -0,0 +1,10 @@
+package com.selenium.PracticOne;
+
+import org.testng.annotations.Test;
+
+public class HellowWorld {
+	@Test
+	public void helloworld() {
+		System.out.println("Helloworld");
+	}
+}
